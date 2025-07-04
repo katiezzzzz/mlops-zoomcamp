bc16e183e58aa9e3b4334db1805c3f9f2f20f458
Katie Zeng <user1@MacBook-Air.local>
2025-07-04 14:03:56 +0100

    test ci

--- web_service/test.py
+++ web_service/test.py
@@ -12,7 +12,7 @@ ride = {"PULocationID": 10, "DOLocationID": 50, "trip_distance": 40}
 url = "http://localhost:9696/predict"
 response = requests.post(url, json=ride, timeout=10)
 print(response.json())
-print("tested")
+print("tested!")
 
 # with open(f'{PATH}/lin_reg.bin', 'rb') as f_in:
 #     (dv, model) = pickle.load(f_in)
